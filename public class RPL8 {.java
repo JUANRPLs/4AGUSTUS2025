83cdc11cce274069897e3ae34eea2c1afb082a69
JUANRPLs <arjuna_mukti_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:43:22 +0700

    Add files via upload
    DATA DIRI

--- public class RPL8 {.java
+++ public class RPL8 {.java
@@ -1,6 +1,16 @@
-public class RPL {
+public class public class RPL8 { 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //main method
+        System.out.println("Arjuna Susila Mukti");
+        System.out.println(x:"SMK TELKOM MALANG");
+        System.out.println("Jurusan: Rekayasa Perangkat Lunak");
+        System.out.println("Kelas: X RPL 8");
+        System.out.println("No Absen: 01");
+        system.out.println(ASAL SMP NEGERI 1 GENTENG
+        System.out.println("NAMA AYAH:AGUSIRIANTO");
+        System.out.println("NAMA IBU:SUSILOWATI");
+        System.out.println("Cita-ciTA: Menjadi Programmer");
+        System.out.println("HOBI: Bermain Game,Sepak Bola");
     }
 
     
